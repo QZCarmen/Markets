ecca7967136e51ca0c65f1f25d48a1061fe723ec
minamary <minam@myumanitoba.ca>
2022-11-27 17:51:32 -0600

    more payment stuff

--- js/payment.js
+++ js/payment.js
@@ -1,45 +1,54 @@
-
 function isNumberKey(evt){
-    var charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
-        return false;
-    return true;
+  var charCode = (evt.which) ? evt.which : evt.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+  return true;
 }
 
 $('#validationCustomUsername').on('keypress change', function() {
-    $(this).val(function(index, value) {
-      return value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
-    });
+  $(this).val(function(index, value) {
+    return value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
   });
+});
 
-  function isValidDate(){
+// function isValidDate(event){
 
-    var today, someday;
-    var exMonth=document.getElementById('selectedMonth');
-    console.log('MONTH:', exMonth.value);
-    var exYear=document.getElementById('selectedYear');
-    console.log('YEAR:', exYear.value);
+//   var today, someday;
+//   var exMonth=document.getElementById('selectedMonth');
+//   console.log('MONTH:', exMonth.value);
+//   var exYear=document.getElementById('selectedYear');
+//   console.log('YEAR:', exYear.value);
 
-    today = new Date();
-    someday = new Date();
-    someday.setFullYear(exYear.value, exMonth.value - 1, someday.getDate());
+//   today = new Date();
+//   someday = new Date();
+//   someday.setFullYear(exYear.value, exMonth.value, someday.getDate());
 
-    if (someday < today) {
-      document.getElementById("errmsg").innerHTML = "Please select a valid expiry date";
-      return false;
-    } else {
-      document.getElementById("errmsg").innerHTML = "";
-      return true;
-    }
+//   if (someday < today) {
+//     // document.getElementById("errmsg").innerHTML = "Please select a valid expiry date";
+//     exMonth.setCustomValidity("tets Please select a valid expiry date");
+//     exYear.setCustomValidity("");
+//     return false;
+//   }
+// }
 
-    // document.getElementById("errmsg") = "You selected: " + x;
-    // today = new Date();
-    // someday = new Date();
-    // someday.setFullYear(exYear, exMonth, 1);
-    
-    // if (someday < today) {
-    //   alert("The expiry date is before today's date. Please select a valid expiry date");
-    //   return false;
-    // }
-  
+$("#selectedYear").bind('blur', function(event) {
+  var today, someday;
+  var exMonth=document.getElementById('selectedMonth');
+  console.log('MONTH1:', exMonth.value);
+  var exYear=document.getElementById('selectedYear');
+  console.log('YEAR1:', exYear.value);
+
+  today = new Date();
+  someday = new Date();
+  someday.setFullYear(exYear.value, exMonth.value - 1 , someday.getDate());
+
+  if (someday < today) {
+    // document.getElementById("errmsg").innerHTML = "Please select a valid expiry date";
+    exYear.setCustomValidity("Please select a valid expiry date");
+    $(".selectedYear").text("Please select a valid expiry date");
+    }else{
+      exYear.setCustomValidity("");
+    return false;
   }
+  
+});
